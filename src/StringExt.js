361f0519e67fdbf56224d8bd174d5191dd623458
Abisoye Oke-lawal <abisoye.oke-lawal@andela.com>
2016-10-04 17:06:56 +0100

    Add the method fromCurrency to StringExt.js

---
+++ src/StringExt.js
@@ -0,0 +1,43 @@
+'use strict';
+var numberInWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six',
+ 'seven', 'eight', 'nine'];
+
+String.prototype.hasVowels = function () {
+  return /[aeiou]/.test(this) ? true : false;
+};
+
+String.prototype.toUpper = function () {
+  return this.replace(/([a-z])/g, function (v) {
+    return String.fromCharCode(v.charCodeAt(0)-32);
+  });
+};
+
+String.prototype.toLower = function () {
+  return this.replace(/([A-Z])/g, function (v) {
+    return String.fromCharCode(v.charCodeAt(0)+32);
+  });
+};
+
+String.prototype.ucFirst = function () {
+  return this.replace(/^[a-z]/, function (v) {
+    return v.toUpper();
+  });
+};
+
+String.prototype.isQuestion = function () {
+  return (/(^[^\?]*\?[^\?]*$)(?!\?$)/.test(this)) ? true : false;
+};
+
+String.prototype.words = function () {
+  return (this).replace(/[^a-zA-Z 0-9]/, '').split(' ');
+};
+
+String.prototype.wordCount = function () {
+  return ((this).replace(/[^a-zA-Z 0-9]/, '').split(' ')).length;
+};
+
+String.prototype.fromCurrency = function () {
+  if (/\d/.test(this)) {
+    return this.replace(/[^\d]/, '');
+  }
+};
