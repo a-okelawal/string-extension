9c37aadb54aaa1788a572093d17bad3477ba2c84
Abisoye Oke-lawal <abisoye.oke-lawal@andela.com>
2016-10-13 14:54:57 +0100

    Expand the end points for some methods listed in the PR

--- spec/StringClassSpec.js
+++ spec/StringClassSpec.js
@@ -32,38 +32,46 @@ describe ('isQuestion should', function () {
   it ('return true if a question and false otherwise.', function () {
     expect('abani?'.isQuestion()).toBe(true);
     expect('lcd'.isQuestion()).toBe(false);
+    expect('can it have? multiple questions?'.isQuestion()).toBe(true);
   });
 });
 
 describe ('words should', function () {
   it ('return a list of words in a string.', function () {
     expect('Abani is a boy'.words()).toEqual(['Abani', 'is', 'a', 'boy']);
+    expect('Abani is a boy 24 times'.words()).toEqual(['Abani', 'is', 'a', 'boy', 'times']);
   });
 });
 
 describe ('wordCount should', function () {
   it ('return the number of words in a string.', function () {
-    expect('abani is a boy'.wordCount()).toEqual(4);
+    expect('abani is a boy times'.wordCount()).toEqual(5);
+    expect('abani is a boy 24 times'.wordCount()).toEqual(5);
   });
 });
 
 describe ('toCurrency should', function () {
   it ('return a currency representation of string.', function () {
     expect('111111.11'.toCurrency()).toBe('111,111.11');
+    expect('111111.1133'.toCurrency()).toBe('111,111.1133');
     expect('1RF,!!!.11'.fromCurrency()).toEqual('String is not in digits.');
+    expect('111111.11FF'.toCurrency()).toBe('String is not in digits.');
   });
 });
 
 describe ('fromCurrency should', function () {
   it ('return a number representation of string.', function () {
     expect('111,111.11'.fromCurrency()).toEqual('111111.11');
-    expect('1RF,!!!.11'.fromCurrency()).toEqual('String is not in digits.');
+    expect('111,111.1144'.fromCurrency()).toEqual('111111.1144');
+    expect('1RF,!!!.1144'.fromCurrency()).toEqual('String is not in digits.');
+    expect('1RF,!!!.11F44'.fromCurrency()).toEqual('String is not in digits.');
   });
 });
 
 describe ('inverseCase should', function () {
   it ('return an inverse of the cases in the string.', function () {
     expect('My Food'.inverseCase()).toBe('mY fOOD');
+    expect('My 34 Food'.inverseCase()).toBe('mY 34 fOOD');
   });
 });
 
@@ -90,6 +98,8 @@ describe ('isDigit should', function () {
   it ('return true if the string is a single digit.', function () {
     expect('7'.isDigit()).toBe(true);
     expect('567'.isDigit()).toBe(false);
+    expect('56f7'.isDigit()).toBe(false);
+    expect('H'.isDigit()).toBe(false);
   });
 });
 
